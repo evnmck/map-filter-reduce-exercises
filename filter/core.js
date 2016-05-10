cc534f584447123e2a00765c21f4f24e3834968b
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-05-10 15:32:13 -0600

    done with filters

--- filter/core.js
+++ filter/core.js
@@ -1,22 +1,50 @@
-function onlyEven (array) {
-  // your code here
+function onlyEven(array) {
+    let newA = array.filter(element => {
+        return element % 2 === 0
+    })
+    return newA
 };
 
-function onlyOneWord (array) {
-  // your code here
+function onlyOneWord(array) {
+    let newA = array.filter(element => {
+        return !element.includes(" ")
+    })
+    return newA
 };
 
-function positiveRowsOnly (array) {
-  // your code here
-};
+function positiveRowsOnly(array) {
+    let newA = array.filter((element) => {
+        let bool = true;
+        element.forEach(el => {
+            if (el < 0) bool = false
+        })
+        return bool
+    })
+    return newA
+}
 
-function allSameVowels (array) {
-  // your code here
+function allSameVowels(array) {
+    let newA = array.filter(element => {
+        let sameVowels = true
+        let twoVowels = []
+        elem = element.split('');
+        elem.forEach(el => {
+            if (el == 'a' || el == 'e' || el == 'i' || el == 'o' || el == 'u')
+                twoVowels.push(el);
+        })
+        for (let i = 0; i <= twoVowels.length - 2; i++) {
+            if (twoVowels[i] !== twoVowels[i + 1]) {
+                sameVowels = false
+            }
+        }
+        return sameVowels
+    })
+    return newA
 };
 
 module.exports = {
-  onlyEven: onlyEven,
-  onlyOneWord: onlyOneWord,
-  positiveRowsOnly: positiveRowsOnly,
-  allSameVowels: allSameVowels
+    onlyEven: onlyEven,
+    onlyOneWord: onlyOneWord,
+    positiveRowsOnly: positiveRowsOnly,
+    allSameVowels: allSameVowels
 };
